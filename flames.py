ecd05e8eeef7c17dbb11eea409d8e9c7c9575cde
Yatheendra-R <yatheendra.r07@gmail.com>
2025-06-03 12:11:28 +0530

    four commit

--- flames.py
+++ flames.py
@@ -4,7 +4,9 @@ from tkinter import ttk
 import random
 from PIL import ImageTk,Image
 
-
+#done
+#done 123
+#done 321
 def display(take,flaimage):
 
         black_screen=Label(window,bg="black",width=110,height=8)
